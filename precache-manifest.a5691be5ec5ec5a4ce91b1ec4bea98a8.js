19b6c39235bd25f5a15461147d9d316fa91cad6e
kristinacarlsen <kristida84@gmail.com>
2020-01-04 22:09:26 -0700

    Updates

---
+++ precache-manifest.a5691be5ec5ec5a4ce91b1ec4bea98a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d9137b14794e35e211e832ad3c892f",
+    "url": "/Trajectory/index.html"
+  },
+  {
+    "revision": "d445e7e82e5ade568d74",
+    "url": "/Trajectory/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "298b8ddfb8f1388aa035",
+    "url": "/Trajectory/static/css/main.e42c16aa.chunk.css"
+  },
+  {
+    "revision": "d445e7e82e5ade568d74",
+    "url": "/Trajectory/static/js/2.6bf45065.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Trajectory/static/js/2.6bf45065.chunk.js.LICENSE"
+  },
+  {
+    "revision": "298b8ddfb8f1388aa035",
+    "url": "/Trajectory/static/js/main.850b6e18.chunk.js"
+  },
+  {
+    "revision": "4131bbbbb35851a865e2",
+    "url": "/Trajectory/static/js/runtime-main.7dc47db5.js"
+  },
+  {
+    "revision": "fbafdd0cd1e52e8db3e876791fa30fdd",
+    "url": "/Trajectory/static/media/christmasCookies.fbafdd0c.jpg"
+  },
+  {
+    "revision": "56cfb2a898ee6fc6b92fa56f86d283f0",
+    "url": "/Trajectory/static/media/goals.56cfb2a8.jpg"
+  },
+  {
+    "revision": "398fdc194e896edfb07f35401f8c0465",
+    "url": "/Trajectory/static/media/learnRest.398fdc19.jpg"
+  }
+]);
